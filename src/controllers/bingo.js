2891d2e986f26e78556ff817d2debf2480ea8013
Yamir <yamir@moopixel.com>
2021-07-24 16:13:56 -0500

    Generating numbers of BINGO

--- src/controllers/bingo.js
+++ src/controllers/bingo.js
@@ -1,49 +1,36 @@
 const { response } = require('express');
 
-let arrB = [];
-let arrI = [];
-let arrN = [];
-let arrG = [];
-let arrO = [];
-
-const recurse = async (arr, cnt, min, max) => {
-  let numberRand = Math.floor(Math.random() * (max - min)) + min;
-
-  if (arr.length < cnt) {
-    if (!arr.includes(numberRand)) {
-      arr.push(numberRand);
-    }
-    await recurse(arr, cnt, min, max);
-  } else return arr;
-};
-
 const generateBingo = async (req, res) => {
-  //   let { event, tickets } = req.body;
-  //   const user = req.user;
   try {
     let numbers = {};
+    let strGame = 'BINGO';
 
-    //B
-    numbers.B = await recurse(arrB, 5, 1, 15);
+    const recursePromise = (arr, cnt, min, max) => {
+      return new Promise((resolve) => {
+        let numberRand = Math.floor(Math.random() * (max - min)) + min;
 
-    //I
-    numbers.I = await recurse(arrI, 5, 16, 30);
+        if (arr.length < cnt) {
+          if (!arr.includes(numberRand)) {
+            arr.push(numberRand);
+          }
+          return resolve(recursePromise(arr, cnt, min, max));
+        } else return resolve(arr);
+      });
+    };
 
-    //N
-    numbers.N = await recurse(arrN, 4, 31, 45);
-
-    //G
-    numbers.G = await recurse(arrG, 5, 46, 60);
-
-    //O
-    numbers.O = await recurse(arrO, 5, 61, 75);
+    for (let i = 0; i < strGame.length; i++) {
+      let min = i * 15 + 1;
+      let max = (i + 1) * 15;
+      let char = strGame.charAt(i);
+      numbers[char] = [];
+      await recursePromise(numbers[char], 5, min, max).then((resp) => {});
+    }
 
     res.json({
       ok: true,
       numbers,
     });
   } catch (error) {
-    console.log(error);
     res.status(500).json({
       ok: false,
       msg: 'Error inesperado... revisar logs',
